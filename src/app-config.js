c506fec1936b6fca6f0a744bbe29f9650339a295
seongjinna <seongjinna@gmail.com>
2022-05-10 15:53:23 +0900

    elasticbeanstalk URL 적용

--- src/app-config.js
+++ src/app-config.js
@@ -3,7 +3,9 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
-    backendHost = "http://localhost:8080"
+    backendHost = "http://localhost:8080";
+} else {
+    backendHost = "http://prod-todo-backend.ap-northeast-2.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
